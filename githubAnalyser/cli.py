bd7fb96d493366ae7a07279bf3d344669f105159
stefantaga <42568744+stefantaga@users.noreply.github.com>
2024-05-01 00:22:56 +0200

    First solution

--- githubAnalyser/cli.py
+++ githubAnalyser/cli.py
@@ -1,20 +1,166 @@
 import argparse
-from github_api import fetch_repository_data
-def analyze_command(args):
+import json
+import github_api as hubAPI
+import time
+import tqdm
+import streamlit as st
+import datetime
+import pandas as pd
+from matplotlib import pyplot as plt 
+import seaborn as sns
+
+def get_file_paths(repository):
+    """Returns an array containing all the file paths of the files in the repository given by the user"""
+    files = hubAPI.get_repository_file_paths(repository)
+    paths=[]
+    for file in files:
+        if (len(paths)>300):
+            break
+        if (file['type']!='file'):
+            filesInside = hubAPI.get_repository_tree(repository,file['sha'])
+            for currentFile in filesInside['tree']:
+                if (len(paths)>10):
+                    break
+                if (currentFile['type']=='blob'):
+                    paths.append(currentFile['path'])
+        else :
+            paths.append(file['path'])
+    return paths 
+
+
+def analyze_command_REST_API(args):
     print("Analyzing repository...")
-    respository_data = fetch_repository_data(args.repository)
-    print(respository_data)
+    date_1_april_2024 = datetime.datetime(2024, 4, 20)
+    date_29_april_2024 =  datetime.datetime(2024, 4, 29)
+    response = hubAPI.commits_in_a_given_period(args.repository,
+                                          1,date_1_april_2024.isoformat(), date_29_april_2024.isoformat())
+    commits = response["response"]
+    last_page_number = int(response["last_page"])
+    authors = [x["login"] for x in hubAPI.get_top_five_contributors(args.repository)]
+    files = get_file_paths(args.repository)
+    finalResult = {}
+    finalResult[(0,0)] = 0 
+    for filePath in files:
+        author_impacts =[]
+        for author in authors:
+            print(str(filePath) + " " + str(author))
+            fileImpact = hubAPI.get_author_file_impact(args.repository,1,filePath,
+                                                       date_1_april_2024.isoformat(), date_29_april_2024.isoformat(),
+                                                       author) 
+            author_impacts.append(fileImpact)
+        for i in range(len(authors)):
+            for j in range(i+1,len(authors)):
+                if not ((i,j) in finalResult):
+                    finalResult[(i,j)] = author_impacts[i]*author_impacts[j]
+                else :
+                    finalResult[(i,j)] = finalResult[(i,j)] + author_impacts[i]*author_impacts[j]
+    maxPair = (0,0)
+    for authorPair in finalResult:
+        if (finalResult[authorPair] > finalResult[maxPair]):
+            maxPair = authorPair
+    print(authors[maxPair[0]],authors[maxPair[1]])
 
+def update_author_pairs(author_pairs,data):
+    file_authors = {} 
+    for node in data["data"]["repository"]["defaultBranchRef"]["target"]["history"]["nodes"]:
+        associatedPullRequests = node["associatedPullRequests"]
+        for fileNode in associatedPullRequests["nodes"]:
+                author = fileNode['author']['login']
+                for file in fileNode["files"]["nodes"]:
+                    if not (file["path"] in file_authors):
+                        file_authors[file["path"]] = {}
+                    if not (author in file_authors[file["path"]]):
+                        file_authors[file["path"]][author] = 0
+                    file_authors[file["path"]][author] = file_authors[file["path"]][author] + 1
+    for file in file_authors:
+        for author1 in file_authors[file]:
+            for author2 in file_authors[file]:
+                if (author1 < author2):
+                    if not ((author1,author2) in author_pairs):
+                        author_pairs[(author1,author2)] = 0
+                    author_pairs[(author1,author2)] = author_pairs[(author1,author2)] + file_authors[file][author1] + file_authors[file][author2]
+
+
+def analyze_command_graphQL_API(repository,start,end):
+
+    isoFormatStart = datetime.datetime(start.year,start.month,start.day,0,0,0).isoformat()
+    isoFormatEnd = datetime.datetime(end.year,end.month,end.day,0,0,0).isoformat()
+    no_pages = 1
+    commit_number = hubAPI.get_number_of_commits_with_graphQLAPI(repository
+                                                                 ,isoFormatStart
+                                                                 ,isoFormatEnd)
+    st.write(f"Total number of commits to be evaluated : {commit_number}")
+    progress_bar = st.progress(0, text= "Current commits status")
+    data = hubAPI.get_commits_with_graphQLAPI(repository,isoFormatStart
+                                                        ,isoFormatEnd)
+    author_pairs = {}
+    update_author_pairs(author_pairs,data)
+    dataHistory = data["data"]["repository"]["defaultBranchRef"]["target"]["history"]
+    end_cursor = dataHistory['pageInfo']['endCursor']
+    has_next_page = dataHistory['pageInfo']['hasNextPage']
+    progress_bar.progress(min(no_pages*50,commit_number)/commit_number , text = "Current commits status")
+    time.sleep(1)
+    no_pages = no_pages+1
+    while has_next_page:
+        data = hubAPI.get_commits_with_graphQLAPI_with_cursor(repository,end_cursor
+                                                                ,isoFormatStart
+                                                                ,isoFormatEnd)
+        update_author_pairs(author_pairs,data)
+        dataHistory = data["data"]["repository"]["defaultBranchRef"]["target"]["history"]
+        end_cursor = dataHistory['pageInfo']['endCursor']
+        has_next_page = dataHistory['pageInfo']['hasNextPage']
+        no_pages = no_pages + 1
+        progress_bar.progress(min(no_pages*50,commit_number)/commit_number 
+                              , text = "Current commits status")
+        time.sleep(1) 
+
+    if (len(author_pairs)!=0):
+        st.title("Number of common commits between developers")
+
+        values = [author_pairs.get(pair) for pair in author_pairs.keys()]
+
+        df = pd.DataFrame({'Developer 1': [pair[0] for pair in author_pairs.keys()],
+                        'Developer 2': [pair[1] for pair in author_pairs.keys()],
+                        'Value': values})
+
+        pivot_table = df.pivot_table(values='Value', index='Developer 1', columns='Developer 2', fill_value=0)
+
+        fig, ax = plt.subplots()
+        sns.heatmap(pivot_table, annot=True, cmap="YlGnBu", fmt='g', ax=ax)
+
+        ax.set_title('Common commits')
+        ax.set_xlabel('Developer 2')
+        ax.set_ylabel('Developer 1')
+
+        st.pyplot(fig)
+    else :
+        st.write("No developers have worked on the same files :(")
 def main():
-    parser = argparse.ArgumentParser(description="GitHub Contribution Analysis CLI Tool")
-    subparsers = parser.add_subparsers(title="Commands", dest="command")
+    st.title("Github analyser for finding most collaborative pairs of developers in a repository")
+    st.text(f"""
+    To use the application , add the github repository in the format used below. 
+    For example, you can insert llvm/llvm-project on which this app was tested.
+    Disclaimers :
+        1. The application is evaluating 50 commits at a time such that it does not 
+           get blocked by the GitHub API
+        2. Do not forget to create a .env environment in the githubAnalyser folder 
+           and add your git hub key correspondingly, otherwise the graphQL API is 
+           not reachable
+    """) 
+    user_input = st.text_input("Enter a github repository in the form 'owner/repository_name':", "")
+    today = datetime.datetime.now()
+    jan_1 = datetime.date(2000, 1, 1)
+    dec_31 = datetime.date(today.year, 12, 31)
 
-    analyze_parser = subparsers.add_parser("analyze", help="Analyze GitHub repository")
-    analyze_parser.add_argument("repository", help="GitHub repository (in the format 'owner/repo')")
-
-    args = parser.parse_args()
-    if args.command == "analyze":
-        analyze_command(args)
+    d = st.date_input(
+        "Select interval to evaluate the repository on",
+        (datetime.date(2024, 4, 16), datetime.date(2024, 4, 30)),
+        jan_1,
+        dec_31,
+        format="MM.DD.YYYY",
+    )
+    if st.button("Submit"):
+        analyze_command_graphQL_API(user_input,d[0],d[1])
 
 if __name__ == "__main__":
     main()
--- githubAnalyser/github_api.py
+++ githubAnalyser/github_api.py
@@ -1,10 +1,176 @@
 import requests
 import os
 from dotenv import load_dotenv
-load_dotenv
+import json 
+load_dotenv()
 
-def fetch_repository_data(repository):
-    url = f"https://api.github.com/repos/{repository}/commits"
-    headers = {"Authorization": f"token {os.getenv('GITHUB_TOKEN')}"}
+from urllib.parse import urlparse, parse_qs
+
+auth = {"Authorization": f"bearer {os.getenv('GITHUB_TOKEN')}"}
+
+def get_top_five_contributors(repository):
+    url = f"https://api.github.com/repos/{repository}/contributors?per_page=5"
+    headers = auth
+    response = requests.get(url,headers = headers)
+    return response.json()
+
+def get_repository_tree(repository,treeSHA):
+    url = f"https://api.github.com/repos/{repository}/git/trees/{treeSHA}?recursive=true"
+    headers = auth
+    response = requests.get(url,headers = headers)
+    return response.json()
+
+def get_repository_file_paths(repository):
+    url = f"https://api.github.com/repos/{repository}/contents"
+    headers = auth
+    response = requests.get(url,headers = headers)
+    return response.json()
+
+def fetch_all_repository_data(repository,page_number):
+    url = f"https://api.github.com/repos/{repository}/commits?per_page=50&page={str(page_number)}"
+    headers = auth
     response = requests.get(url, headers=headers)
+    print(response.links)
+    last_page_number = 1
+    if (response.links):
+        parsed_url_last = urlparse(response.links['last']['url'])
+        last_page_number  = parse_qs(parsed_url_last.query)['page'][0]
+    return {"response": response.json(), "last_page" : last_page_number}
+
+def get_specific_commit_info(repository,commitRef):
+    url = f"https://api.github.com/repos/{repository}/commits/{commitRef}"
+    headers = auth
+    response = requests.get(url,headers = headers)
+    return response.json()
+def commits_in_a_given_period(repository,page_number,start,end):
+    url = f"https://api.github.com/repos/{repository}/commits?per_page=100&page={str(page_number)}&since={str(start)}&until={str(end)}"
+    headers = auth
+    response = requests.get(url,headers = headers)
+    last_page_number = 1
+    if (response.links):
+        parsed_url_last = urlparse(response.links['last']['url'])
+        last_page_number  = parse_qs(parsed_url_last.query)['page'][0]
+    return {"response": response.json(), "last_page" : last_page_number}
+def get_author_file_impact(repository,page_number,filePath,start,end,author):
+    url = f"https://api.github.com/repos/{repository}/commits?per_page=1&page={str(page_number)}&path={str(filePath)}&since={str(start)}&until={str(end)}&author={str(author)}"
+    headers = auth
+    response = requests.get(url, headers=headers)
+    last_page_number = 1
+    if (response.links):
+        parsed_url_last = urlparse(response.links['last']['url'])
+        last_page_number  = parse_qs(parsed_url_last.query)['page'][0]
+    return int(last_page_number)
+
+def get_number_of_commits_with_graphQLAPI(repository,start,end):
+    query = """
+    {
+  repository(owner: "owner_username", name: "repository_name") {
+    defaultBranchRef {
+      target {
+        ... on Commit {
+          history(since: "start", until: "end") {
+            totalCount
+          }
+        }
+      }
+    }
+  }
+}
+
+    """
+    query = query.replace("owner_username",repository.split('/')[0])
+    query = query.replace("repository_name", repository.split('/')[1])
+    query = query.replace("start",str(start))
+    query = query.replace("end", str(end))
+    print(query)
+    response = requests.post('https://api.github.com/graphql', json={'query': query}, headers=auth)
+
+    return response.json()["data"]["repository"]["defaultBranchRef"]["target"]["history"]["totalCount"]
+
+def get_commits_with_graphQLAPI(repository,start,end):
+    query = """
+    {
+  repository(owner: "owner_username", name: "repository_name") {
+    defaultBranchRef {
+      target {
+        ... on Commit {
+          history(since: "startTT", until: "endTT", first: 50) {
+            pageInfo {
+            endCursor
+            hasNextPage
+            }
+            nodes{
+                associatedPullRequests(first:25){
+                    nodes{
+                        files(first:25){
+                            nodes{
+                                path
+                            }
+                        }
+                        author{
+                            login
+                        }
+                    }
+                }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+    """
+    
+    query = query.replace("owner_username",repository.split('/')[0])
+    query = query.replace("repository_name", repository.split('/')[1])
+    query = query.replace("startTT",str(start))
+    query = query.replace("endTT", str(end))
+    response = requests.post('https://api.github.com/graphql', json={'query': query}, headers=auth)
+
+    return response.json()
+
+
+def get_commits_with_graphQLAPI_with_cursor(repository,after_cursor,start,end):
+    query = """
+    {
+  repository(owner: "owner_username", name: "repository_name") {
+    defaultBranchRef {
+      target {
+        ... on Commit {
+          history(since: "startTT", until: "endTT", first: 50 , after:"after_cursor") {
+            pageInfo {
+            endCursor
+            hasNextPage
+            }
+            nodes{
+                associatedPullRequests(first:25){
+                    nodes{
+                        files(first:25){
+                            nodes{
+                                path
+                            }
+                        }
+                        author{
+                            login
+                        }
+                    }
+                }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+    """
+    
+    query = query.replace("owner_username",repository.split('/')[0])
+    query = query.replace("repository_name", repository.split('/')[1])
+    query = query.replace("after_cursor", after_cursor)
+    query = query.replace("startTT",str(start))
+    query = query.replace("endTT", str(end))
+
+    response = requests.post('https://api.github.com/graphql', json={'query': query}, headers=auth)
     return response.json()
